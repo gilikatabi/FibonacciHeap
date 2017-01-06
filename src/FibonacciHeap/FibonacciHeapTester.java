5ae4b66a4ceabb26c13dca32946f1d4ff159d11c
gilikatabi <gilikatabi@gmail.com>
2017-01-06 10:51:53 +0200

    test insert and delete min

--- src/FibonacciHeap/FibonacciHeapTester.java
+++ src/FibonacciHeap/FibonacciHeapTester.java
@@ -1,10 +1,38 @@
 package FibonacciHeap;
 
 import org.junit.Test;
-
+import org.junit.Assert;
 
 public class FibonacciHeapTester {
 	@Test
-	public void TestSomething() {
+	public void TestInsertAndDeleteMin() {
+		FibonacciHeap heap = new FibonacciHeap();
+		heap.insert(9);
+		heap.insert(2);
+		heap.insert(47);
+		heap.insert(13);
+		heap.insert(7);
+		heap.insert(5);
+		heap.insert(22);
+		heap.insert(1);
+		heap.insert(83);
+		Assert.assertEquals(1, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(2, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(5, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(7, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(9, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(13, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(22, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(47, heap.findMin());
+		heap.deleteMin();
+		Assert.assertEquals(83, heap.findMin());
+		heap.deleteMin();		
 	}
 }
